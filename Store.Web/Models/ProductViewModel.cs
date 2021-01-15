2b1c2988212aa77a8317a797907a9656b90ccb41
Diogo Machado <Diogo.Neves.Machado@formandos.cinel.pt>
2021-01-15 16:46:44 +0000

    Add and Edit images

---
+++ Store.Web/Models/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Store.Web.Models
+{
+    using Microsoft.AspNetCore.Http;
+    using Store.Web.Data.Entidades;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ProductViewModel : Product
+    {
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+    }
+}
